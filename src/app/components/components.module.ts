a556ac75727b5697f8167ed1eccccf697ceb3bce
jair <jairdeyner730791@gmail.com>
2023-01-04 10:08:46 -0500

    Diseño del formulario y la lista agregadas

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormTareaComponent } from './form-tarea/form-tarea.component';
+import { ListaTareasComponent } from './lista-tareas/lista-tareas.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FormTareaComponent,
+    ListaTareasComponent
+  ],
+  exports: [
+    FormTareaComponent,
+    ListaTareasComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ src/app/components/form-tarea/form-tarea.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-tarea',
+  templateUrl: './form-tarea.component.html',
+  styles: [
+  ]
+})
+export class FormTareaComponent {
+
+}
